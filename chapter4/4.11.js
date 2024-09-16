95c4e0cc8bd3df17bb3e3ef82c857c985de63372
neu-k <rsk367277@gmail.com>
2024-09-17 08:46:27 +0900

    context.strokeRect()

---
+++ chapter4/4.11.js
@@ -0,0 +1,8 @@
+let canvas = document.getElementById("main_canvas");
+let context = canvas.getContext("2d");
+
+context.fillStyle = "#c5c8da";
+context.fillRect(50, 50, 50, 50);
+
+context.strokeStyle = "#e5d9d0";
+context.strokeRect(10, 10, 30, 30);
